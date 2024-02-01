0bf376b867cc4a9e0a943a0a5a2e1de3331b111f
merp <reesec@daredevils2512.org>
2024-02-01 16:13:08 -0600

    shooter

--- src/main/java/frc/robot/subsystems/ShooterSub.java
+++ src/main/java/frc/robot/subsystems/ShooterSub.java
@@ -5,12 +5,16 @@ import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+
 import frc.robot.Constants;
 
 public class ShooterSub extends SubsystemBase {
 
   private final WPI_VictorSPX shooterMotor;
   private final WPI_VictorSPX shooterActuateMotor;
+ // private final DoubleSolenoid pneumatics;
   private final DigitalInput limitSwitchTop;
   private final DigitalInput limitSwitchBottom;
   private final Encoder shooterEncoder;
@@ -19,6 +23,7 @@ public class ShooterSub extends SubsystemBase {
     shooterMotor = new WPI_VictorSPX(Constants.ShooterConstants.SHOOTER_MOTOR_ID);
     shooterActuateMotor = new WPI_VictorSPX(Constants.ShooterConstants.SHOOTER_ACTUATE_MOTOR_ID);
 
+    //pneumatics = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, );
     limitSwitchTop = new DigitalInput(Constants.ShooterConstants.SHOOTER_LIMIT_SWITCH_TOP);
     limitSwitchBottom = new DigitalInput(Constants.ShooterConstants.SHOOTER_LIMIT_SWITCH_BOTTOM);
     shooterEncoder = new Encoder(Constants.ShooterConstants.SHOOTER_ENCODER_A, Constants.ShooterConstants.SHOOTER_ENCODER_B);
