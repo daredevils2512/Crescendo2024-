6dd8d00fe141d56fe88d595c674cd2ee2b631820
merp <reesec@daredevils2512.org>
2024-02-24 13:21:47 -0600

    made the auto selection better

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -14,6 +14,9 @@ import frc.robot.subsystems.IntakeSub;
 import frc.robot.subsystems.PIDTest;
 import frc.robot.subsystems.ShooterSub;
 import frc.robot.subsystems.ShooterSub.Position;
+
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.cameraserver.CameraServer;
 import edu.wpi.first.cscore.MjpegServer;
 import edu.wpi.first.cscore.UsbCamera;
@@ -31,24 +34,21 @@ public class RobotContainer {
   private final ClimberSub climberSub = new ClimberSub();
   private final Extreme extreme = new Extreme(1); // constants??
   private final CommandXboxController xbox = new CommandXboxController(IoConstants.XBOX_CONTROLLER_PORT);
-  private static final String simpleAuto = "move forward";
-  private static final String fullAuto = "full auto";
-  private static final String middleAuto = "middle auto";
-  private static final String farAuto = "far auto";
-  private String autoSelected;
-  private final SendableChooser<String> chooser = new SendableChooser<>();
+  private enum Auto {
+    None, DriveForward, FullAuto, SpeakerAuto, StageAuto
+  }
+  private final SendableChooser<Auto> chooser = new SendableChooser<>();
   private final PIDTest pid = new PIDTest();
 
   public RobotContainer() {
     configureBindings();
 
-    chooser.setDefaultOption("move forward", simpleAuto);
-    chooser.addOption("full auto", fullAuto);
-    chooser.addOption("middle auto", middleAuto);
-    chooser.addOption("far Auto", farAuto);
-    SmartDashboard.putData("Auto choices", chooser);
-    autoSelected = chooser.getSelected();
-    System.out.println("Auto selected: " + autoSelected);
+    chooser.setDefaultOption("Move Forward", Auto.DriveForward);
+    chooser.addOption("None", Auto.None);
+    chooser.addOption("Full Auto", Auto.FullAuto);
+    chooser.addOption("Speaker Auto", Auto.SpeakerAuto);
+    chooser.addOption("Stage Auto", Auto.StageAuto);
+    SmartDashboard.putData("Auto", chooser);
 
     UsbCamera intakeCamera = CameraServer.startAutomaticCapture(1);
     intakeCamera.setResolution(640, 480);
@@ -76,7 +76,9 @@ public class RobotContainer {
   private void configureBindings() {
 
     // Drivetrain
-    driveSub.setDefaultCommand(driveSub.run(() -> driveSub.arcadeDrive(-xbox.getLeftY(), xbox.getLeftX())));
+    DoubleSupplier move = () -> MathUtil.applyDeadband(-xbox.getLeftY(), 0.1);
+    DoubleSupplier turn = () -> MathUtil.applyDeadband(xbox.getLeftX(), 0.1);
+    driveSub.setDefaultCommand(driveSub.run(() -> driveSub.arcadeDrive(move.getAsDouble(), turn.getAsDouble())));
     xbox.leftBumper().onTrue(DriveCommands.toggleInverted(driveSub));
 
     // xbox.a().whileTrue(pid.run(()-> pid.setTargetPosition(10)));
@@ -91,21 +93,19 @@ public class RobotContainer {
     shooterSub.setDefaultCommand(shooterSub.run(() -> shooterSub.runShooterActuate(extreme.getStickY())));
     extreme.trigger.whileTrue(ShooterCommands.runShooterOut(shooterSub, 1)); // shoot
     extreme.sideButton.whileTrue(ShooterCommands.runShooterIn(shooterSub, -1)); // intake to shooter
-    extreme.baseFrontLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Top, 0.6));
-    extreme.baseMiddleLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Handoff, 0.6));
-    extreme.baseBackLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Bottom, 0.6));
+    // extreme.baseFrontLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Top, 0.6));
+    // extreme.baseMiddleLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Handoff, 0.6));
+    // extreme.baseBackLeft.onTrue(ShooterCommands.setPosition(shooterSub, Position.Bottom, 0.6));
 
     // Climber
     extreme.joystickTopRight.whileTrue(ClimberCommands.runClimberUp(climberSub, 0.5));
     extreme.joystickTopLeft.whileTrue(ClimberCommands.runClimberUp(climberSub, -0.5));
   }
 
-  public void autonomousCommand() {
-  switch (autoSelected) {
-      case fullAuto:
-        // !!!! please test this before running it, idk if it will break the robot or if it even works
-
-         AutoCommands.autoDriveAndTurn(driveSub, 0.5, 2) // theoretically be at amp
+  public Command autonomousCommand() {
+  switch (chooser.getSelected()) {
+      case FullAuto:
+          return AutoCommands.autoDriveAndTurn(driveSub, 0.5, 2) // theoretically be at amp
             // return AutoCommands.AutoDriveForward(driveSub, 0.5, 1) // use this if ^ doesnt work
             // .andThen(AutoCommands.AutoDriveLeft(driveSub, 0.5, 1))
             .andThen(ShooterCommands.runTimedShooterActuate(shooterSub, 0.5, 2)) // change time till it gets to the top
@@ -113,20 +113,22 @@ public class RobotContainer {
             .andThen(ShooterCommands.runTimedShooterActuate(shooterSub, -0.5, 2)) // maybe put after moving, might slam into the amp
             .andThen(AutoCommands.autoDriveAndTurn(driveSub, -0.5, 1))
             .andThen(IntakeCommands.runIntakeToShooter(intakeSub, shooterSub, 1).withTimeout(1.3)); // intake another ring
-        break;
 
-      case middleAuto:
-         AutoCommands.autoDriveForward(driveSub, 0.5, 1);
-        break;
+      case SpeakerAuto:
+        return  AutoCommands.autoDriveForward(driveSub, 0.3, 1);
+      
 
-      case farAuto: // make sure this is tuned so it doesnt slam into the stage
-        AutoCommands.autoDriveForward(driveSub, 0.5, 1.5);
-        break;
+      case StageAuto: 
+        return AutoCommands.autoDriveForward(driveSub, 0.4, 1.5);
+      
 
-      case simpleAuto:
+      case DriveForward:
+        return AutoCommands.autoDriveForward(driveSub, 0.5, 2);
+       
+
+      case None:
       default:
-        AutoCommands.autoDriveForward(driveSub, 0.5, 2);
-        break;
+        return new Command(){};
     }
   }
 }
